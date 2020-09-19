adee0b81a363983a5ef4833ccf7b6c59692da014
kudo1521 <agongteng73@gmail.com>
2020-09-19 13:11:16 +0900

    if文を作ろうとしている

--- comon.js
+++ comon.js
@@ -2,4 +2,7 @@ $(function(){
     $("#question-list dt").on("click", function() {
     $(this).next().slideToggle();
     });
+    if($('#question-list dt').hasclass('.under')){
+        $('#question-list dt').removeclass('.under');
+    };
 });
