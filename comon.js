316e0b20ca8cf78e83ccbf6c2ee13cb81fbb49ae
kudo1521 <agongteng73@gmail.com>
2020-09-19 16:16:47 +0900

    △の修正、質問している文章をコードにしている。

--- comon.js
+++ comon.js
@@ -3,8 +3,8 @@ $(function(){
     $(this).next().slideToggle();
     });
     $("#question-list dt").click(function(){
-        if($(span).hasClass('under')){
-            $(this).children(span).removeClass('under')
+        if($("#question-list").hasClass('under')){
+            $(this).children().removeClass('under')
         };
     });
 });
